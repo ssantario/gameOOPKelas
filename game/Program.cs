23056cd11e5de78140f31330d5d36f5769efef33
Javen <javana.dz@gmail.com>
2024-11-11 13:39:22 +0700

    lanjutin ni lil

--- game/Program.cs
+++ game/Program.cs
@@ -6,11 +6,27 @@ namespace game
     {
         public static void Main(string[] args)
         {
-            Character hero = Character.GetInstance("Hero", 100, 15);
-            Enemy goblin = EnemyFactory.CreateEnemy("Goblin");
+            ShowMainMenu();
 
-            hero.Attack(goblin);
-            goblin.Attack(hero);
+            Character hero = Character.GetInstance("Hero", 100, 15);
+            Enemy orcWarrior = EnemyFactory.CreateEnemy("Orc", "Warrior");
+
+            hero.Attack(orcWarrior);
+            orcWarrior.Attack(hero);
+            hero.Attack(orcWarrior);
+            orcWarrior.Attack(hero);
+        }
+
+        private static void ShowMainMenu()
+        {
+            Console.WriteLine(@"-_-/        ,, ,,                                 ,         
+            (_ /         || ||                 '         _    ||         
+            (_ --_   /'\\ || || \\ \\ \\/\\/\\ \\ \\/\\  < \, =||= '\\/\\ 
+            --_ ) || || || || || || || || || || || ||  /-||  ||   || ;' 
+            _/  )) || || || || || || || || || || || || (( ||  ||   ||/   
+            (_-_-   \\,/  \\ \\ \\/\\ \\ \\ \\ \\ \\ \\  \/\\  \\,  |/    
+                                                                (      
+                                                                    -_- ");
         }
     }
 }
